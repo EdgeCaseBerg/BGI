8d353923da375ef307a3b081401c8b8f32436e51
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-02 08:49:54 -0400

    Added sample configuration file.
    And ignoring actual one. This means that before you compile you'll need
    to create a configuration file. Maybe we'll write a shell script or
    something that will configure everything for you, who knows!

---
+++ headers/sample-config.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_H__
+	#define __CONFIG_H__
+	#define USER "guest"
+	#define PASSWORD "dev"
+	
+	#define DATABASE_HOST "localhost"
+	#define DATABASE_NAME "bgi"
+	#define DATABASE_USER "bgi"
+	#define DATABASE_PASS "bgi"
+
+#endif
