aa203e552dbeba05063bc71d2541674718fb59a6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-07 08:40:41 -0400

    Changed listings to tables. Updated Js.
    Makes more sense for the accounts and line items to appear in a table,
    as it is tabular data.
    
    Updated JS to load the accounts from the cgi script.

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,18 @@ jQuery( document ).ready(function( $ ) {
 	/* Load accounts */
 	if($('body').attr('id') == "welcome"){
 		var accountsURI =  window.bgidomain + "accounts.cgi"
-		console.log( $('ul[name="accounts-list"]').get(accountsURI) )
+		$.get(accountsURI, function(response){
+			var accounts = response
+			for (var i = accounts.length - 1; i >= 0; i--) {
+				var row = $('<tr></tr>')
+				var first = $('<td>' + accounts[i].id + '</td>')
+				var second = $('<td>' + accounts[i].name + '</td>')
+				var third = $('<td>' + accounts[i].balance + '</td>')
+				row.append(first)
+				row.append(second)
+				row.append(third)
+				$('table[name="accounts-list"]').find('tbody').append(row)
+			};
+		})
 	}
 })
