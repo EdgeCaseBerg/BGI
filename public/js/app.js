6996c6576b4cced06df1a92f8d90751572d35cfb
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-07 17:52:35 -0400

    Added load line items button.
    Added button to view an account, need to create the CGI script that will
    return each line item to the user. Added skeleton JS around where the
    line items will be retrieved and loaded onto the page.

--- public/js/app.js
+++ public/js/app.js
@@ -6,14 +6,29 @@ jQuery( document ).ready(function( $ ) {
 			var accounts = response
 			for (var i = accounts.length - 1; i >= 0; i--) {
 				var row = $('<tr></tr>')
+				var button = $('<td><button name="view-account" rel="'+accounts[i].name+'">View</button></td>')
 				var first = $('<td>' + accounts[i].id + '</td>')
 				var second = $('<td>' + accounts[i].name + '</td>')
 				var third = $('<td>' + accounts[i].balance + '</td>')
+				row.append(button)
 				row.append(first)
 				row.append(second)
 				row.append(third)
 				$('table[name="accounts-list"]').find('tbody').append(row)
 			};
+		})
+
+		$('table[name="accounts-list"]').on('click', 'button[name="view-account"]', function(evt){
+			evt.preventDefault()
+			/* Load the line items for this account */
+			var accountName = $(this).attr('rel')
+			$('section[name="account-display"]').find('h3').text(accountName.charAt(0).toUpperCase() + accountName.substr(1))
+
+			/* Get em. */
+			var lineItemsTable = $('table[name="lineitems"]')
+			var lineItemsURI = window.bgidomain + "lineitems.cgi?account=" + accountName
+
+			console.log(lineItemsURI)
 		})
 	}
 })
