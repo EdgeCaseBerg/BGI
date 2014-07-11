36ece18187745c4db7a72bbac306a7b62096f9f6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-11 08:26:34 -0400

    Fixed test for line items.
    No leaks. and no invalid reads or writes. Time to apply it to ctl.c

--- test/src/test-list-lineitems.c
+++ test/src/test-list-lineitems.c
@@ -47,19 +47,26 @@ int main(){
 	chain = malloc(sizeof(struct lineItemChain));
 	if(chain == NULL){
 		fprintf(stderr, "%s %s, line: %d\n", OUT_OF_MEMORY, __FILE__, __LINE__);
+		fclose(fp);
 		return 0; /* OUT OF MEMORY */	
 	}
+	chain->data = NULL;
 	chain->next = NULL;
 	head = chain;
 	backPtr = head;	
 	
-	char * name = NULL;
+	char * name = malloc(sizeof(char)* 255 );
+	if(name == NULL){
+		fclose(fp);
+		free(chain);
+		return 0;
+	}
 	time_t date;
 	long tmpdate;
 	double amount;
 	double latitude;
 	double longitude;
-	while(fscanf(fp, "%ld %s %lf %lf %lf\n", &tmpdate, name, &amount, &latitude, &longitude) == 5){
+	while(fscanf(fp, "%ld %255[^0-9] %lf %lf %lf\n", &tmpdate, name, &amount, &latitude, &longitude) == 5){
 		date = tmpdate;
 		chain->data = malloc(sizeof(struct lineitem));
 		if(chain->data == NULL){
@@ -72,10 +79,17 @@ int main(){
 		fprintf(stderr, "%zu %s %lf %lf %lf\n", date, name, amount, latitude, longitude);
 
 		chain->data->date = date;
-		chain->data->name = name;
+		chain->data->name = malloc(sizeof(char)*255);
+		if(chain->data->name == NULL){
+			fclose(fp);
+			goto destroy_list;
+		}
+		memcpy(chain->data->name, name, 255);
 		chain->data->amount = amount;
 		chain->data->latitude = latitude;
 		chain->data->longitude = longitude;
+
+		fprintf(stderr, "%zu %s %lf %lf %lf\n", chain->data->date, chain->data->name, chain->data->amount, chain->data->latitude, chain->data->longitude);
 
 		chain->next = malloc(sizeof(struct lineitem));
 		if(chain->next == NULL){
@@ -85,9 +99,10 @@ int main(){
 		}
 
 		backPtr = chain;
-		free(backPtr);
 		chain = chain->next;
 	}
+	//Free the space used to allocate for the string
+	free(name);
 
 	/* We end up freeing one more item on the list than neccesary, so free that 
 	 * up and NULL the ->next from the back pointer
@@ -96,19 +111,18 @@ int main(){
 		free(backPtr->next);
 		backPtr->next = NULL;
 	}
-	fclose(fp);
-
-	return 1;
+	fclose(fp);	
 
 	destroy_list:
 	fprintf(stderr, "%s\n", "destroying list");
 	if(head != NULL){
 		for (chain = head; chain != NULL; ){
+			if(chain->data->name != NULL) free(chain->data->name);
 			if(chain->data != NULL) free(chain->data);
 			head = chain->next;
 			free(chain);
 			chain = head;
 		}
 	}
-	return 0;
+	return 1;
 }
