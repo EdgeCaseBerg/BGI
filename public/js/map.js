ce3d24afdb3663b72ccf4515287ae949704bc0f6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-14 18:24:09 -0400

    Added n2c function and cdn for leaflet.
    For map page, updated layout with leaflet cdn links, also created small
    and simple function to convert an account name into a color to be used.
    Right now I dont have many colors in place in the list, but really, it's
    pretty simple to extend and I'm not concerned yet.

--- public/js/map.js
+++ public/js/map.js
@@ -3,8 +3,18 @@ jQuery( document ).ready(function( $ ) {
      * that way it is easy to discern where and how the money is being
      * spent.
 	*/
+	
 	var accountsURI =  window.bgidomain + "accounts.cgi"
 	var lineItemsURI = window.bgidomain + "list-lineitems.cgi?accountname=" /* + accountName */
+
+	function n2c(name){
+		var colors = ['red','blue','yellow','green','purple']
+		var color = 1;
+		for(idx in name){
+			color += name.charCodeAt(idx)*idx
+		}
+		return colors[color % colors.length];
+	}
 
 	$('#themap').css({'opacity' : .5})
 
@@ -14,23 +24,41 @@ jQuery( document ).ready(function( $ ) {
     	maxZoom: 18
 	}).addTo(map);
 
-	window.accounts = []
-	window.items = []
-
 	$.get(accountsURI, function(accounts){
 		for (var i = accounts.length - 1; i >= 0; i--) {
+			var accountName = accounts[i].name
 			$.get(lineItemsURI + accounts[i].name, function(lineitems){
+				var items = {}
 				for (var i = lineitems.length - 1; i >= 0; i--) {
 					var item = lineitems[i]
-					var circle = L.circle([item.latitude, item.longitude], 500, {
-	    				color: 'red',
-	    				fillColor: '#f03',
-	    				fillOpacity: 0.5
-					}).addTo(map);
-					var name = item.name
-					var amt = item.amount
-					circle.bindPopup("<div>" + name + "<br/>" + amt  + "</div>");
+					if( items[item.latitude] ){
+						if(items[item.latitude][item.longitude]){
+							items[item.latitude][item.longitude]["amt"] += item.amount
+							items[item.latitude][item.longitude]["cnt"] += 1 
+						}else{
+							items[item.latitude][item.longitude] = {"amt" : item.amount, "cnt" : 1}
+						}
+					}else{
+						items[item.latitude] = {}
+						items[item.latitude][item.longitude] = {"amt" : item.amount, "cnt" : 1}
+					}
 				};
+
+				/* Now we have an object keyed by latitude, who holds objects keyed by longitude, 
+				 * that hold the aggregate sum of their amounts and how many there are.
+				*/
+				for(latitude in items){
+					for(longitude in items[latitude] ){
+						var item = items[latitude][longitude]
+						var circle = L.circle([latitude, longitude], 500, {
+		    				color: n2c(accountName),
+		    				fillColor: '#f03',
+		    				fillOpacity: 0.5
+						}).addTo(map);
+						circle.bindPopup("<div>" + item.amt+ "Spent<br/>" + item.cnt  + " Items<br/>"+(item.amt/item.cnt) +" Avg.</div>");
+					}
+				}
+				$('#themap').fadeIn()
 			})
 		};
 		$('#themap').fadeIn()
