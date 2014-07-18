f39ce1b1e0f165cd4fe5ca20bb7c23777ee46e13
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-07-18 14:23:42 -0400

    Compiled timeline.

--- www/js/timeline.js
+++ www/js/timeline.js
@@ -10,8 +10,11 @@ jQuery( document ).ready(function( $ ) {
 		return colors[color % colors.length];
 	}
 	
-	var datasets = []
-	var labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	var monthDataSets = []
+	var monthLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+	var dayDataSets = []
+	var dayLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 	/* Retrieve and create the timeline for each month */
 	$.get(timelineURI, function(timeline){
@@ -19,29 +22,47 @@ jQuery( document ).ready(function( $ ) {
 			var accountName = timeline[i].name
 			
 			/* Initialize data counts*/
-			var data = []
-			for (var k= labels.length - 1; k >= 0; k--) {
-				data.push(0)
+			var monthData = []
+			var dayData = []
+			for (var k= monthLabels.length - 1; k >= 0; k--) {
+				monthData.push(0)
+			};
+			for (var k = dayLabels.length - 1; k >= 0; k--) {
+				dayData.push(0)
 			};
 			var label = accountName
 			var color = n2c(accountName)
 
 			for (var j = 0; j < timeline[i].items.length; j++) {
-				data[new Date(parseInt(timeline[i].items[j].date)*1000).getMonth()] += ( timeline[i].items[j].amount )
+				monthData[new Date(parseInt(timeline[i].items[j].date)*1000).getMonth()] += ( timeline[i].items[j].amount )
+				dayData[new Date(parseInt(timeline[i].items[j].date)*1000).getDay()] += ( timeline[i].items[j].amount )
 			};
 
-			datasets.push({
+			monthDataSets.push({
 				label: accountName,
 				fillColor: color,
-				data: data
+				data: monthData
 			})
-		
+			
+			dayDataSets.push({
+				label: accountName,
+				fillColor: color,
+				data: dayData
+			})
 		}
-		var data = {
-    		labels: labels,
-	    	datasets: datasets
+		var monthData = {
+    		labels: monthLabels,
+	    	datasets: monthDataSets
     	}
-    	var ctx = document.getElementById("timelinecanvas").getContext("2d");
-    	var myLineChart = new Chart(ctx).Line(data, {});
+    	var mctx = document.getElementById("monthcanvas").getContext("2d");
+    	var myLineChart = new Chart(mctx).Bar(monthData, {});
+
+    	var dayData = {
+    		labels: dayLabels,
+    		datasets: dayDataSets
+    	}
+		var dctx = document.getElementById("daycanvas").getContext("2d");
+    	var myLineChart = new Chart(dctx).Bar(dayData, {});
+
 	})
 })
