e914a5adc7fd86aba5a4429b0b6a3bb4c3862660
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-08-05 21:42:22 -0400

    Constructing Month view for comparisons.
    Need to create pie chart and list out items. and then after that we will
    approach the more difficult to display week view. (more difficult
    because after all, how does one decide how a day is in a given week?)

--- public/js/compare.js
+++ public/js/compare.js
@@ -1,1 +1,64 @@
-compare.js
+var templateNum = 0
+var timelineURI = window.bgidomain + "timeline.cgi"
+window.timeline = []
+
+var monthDataSets = []
+var monthLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+var dayDataSets = []
+var dayLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+var pieDataSets = {}
+var timelineDateIndexed = {}
+
+
+function makeNewTemplate(){
+	templateNum++
+	cpy = $('#template').clone()
+	cpy.attr('id', 'cpy' + templateNum)
+	return cpy;
+}
+
+function setupMonthData(){
+	templateNum = 0;
+	$('compareItem:not(#template)').remove()
+	for (var i = 0; i < monthLabels.length; i++) {
+		var lbl = monthLabels[i]
+		tmpl = makeNewTemplate()
+		tmpl.find('[name=title]').text(lbl)
+		//Use dataset to populate pie and items list
+		tmpl.fadeIn().css('display','')
+
+		$('section').append(tmpl)
+	};
+	$('compareItem:not(#template)').fadeIn()
+}
+
+function setup(){
+	$('name[byweek]').click(function(){
+		
+		console.log(window.timeline)
+	})
+	$('name[bymonth]').click(function(){
+		setupMonthData()	
+	})
+	$('section').on('click','[name="itemcontainer"] button', function(){
+		var items = $(this).parent().find('[name="items"]')
+		items.slideToggle()
+		$(this).text('Toggle Item Display')
+
+	})
+}
+	
+jQuery( document ).ready(function( $ ) {
+	//use the timeline data since it's in the closest form we want to work with
+	
+setup()
+	$.get(timelineURI, function(timeline){
+		window.timeline = timeline
+		setup()
+	}).error(function(evt){
+		console.error(evt)
+		//throw "Could not load data for comparisons!"
+	})
+})
