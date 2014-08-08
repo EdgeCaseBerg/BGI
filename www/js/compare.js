554e476968b35329f8f670b5a186ed6c52ba1db8
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-08-08 19:53:04 -0400

    Compiled Harp.
    Compiled Harp to deploy week view.

--- www/js/compare.js
+++ www/js/compare.js
@@ -6,7 +6,15 @@ var monthLabels = ["January", "February", "March", "April", "May", "June", "July
 var dayLabels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 var weekPieDataSets = []
-weekPieDataSets = []
+var weekPieLables = []
+
+//http://stackoverflow.com/a/6117889/1808164
+Date.prototype.getWeekNumber = function(){
+    var d = new Date(+this);
+    d.setHours(0,0,0);
+    d.setDate(d.getDate()+4-(d.getDay()||7));
+    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
 
 var monthDataItems = []
 var monthPieDataSets = []
@@ -70,10 +78,65 @@ function setupWeekData(){
 	templateNum = 0;
 	clearTemplates()
 	//compute week by week for each item
+	//weekPieDataSets// array of arrays containing items per week
+	//weekPieLables // array of "Week of <>""
+
+	for (var k = weekPieLables.length - 1; k >= 0; k--) {
+		var title = weekPieLables[k]
+		var data = weekPieDataSets[k]
+		if(data.length <= 0)
+			continue
+
+		var tmpl = makeNewWeekTemplate()
+		tmpl.find("[name=title]").text(title)
+
+		/* Create data for pie chart: */
+		weekByAccountDataSet = []
+		var labels = []
+		var dataForLabel = {}
+
+		for (var i = data.length - 1; i >= 0; i--) {
+			var item = data[i]
+			if(item.account in dataForLabel){
+				dataForLabel[item.account] += item.amount
+			}else{
+				labels.push(item.account)
+				dataForLabel[item.account] = item.amount
+			}
+		};
+
+		for (var i = labels.length - 1; i >= 0; i--) {
+			var label = labels[i]
+			weekByAccountDataSet.push({
+				value: dataForLabel[label],
+				color: n2c(label),
+				highlight: n2c(label + "a"),
+				label: label
+			})
+		};
+		
+		
+		var pieCanvas = tmpl.find('[name=pie]')[0].getContext("2d")
+		var chart = new Chart(pieCanvas).Pie(weekByAccountDataSet, {})
+
+		list = tmpl.find('[name=items]')
+		var total = 0;
+		for(idx in data){
+			var item = data[idx]
+			total += item.amount
+			list.append("<li><span>"+item.name+"</span><span class=\"amount\">$"+item.amount.toFixed(2)+"</span></li>")
+		}
+		tmpl.find('[name=total]').text("Total: $" + total.toFixed(2))
+		$('section').append(tmpl)
+		
+		tmpl.fadeIn().css('display','')
+	};
+
 	showTemplates()
 }
 
 function setup(){
+	var items = []
 	for (account in window.timeline) {
 		//timeline is by account
 		var account = window.timeline[account]
@@ -85,6 +148,8 @@ function setup(){
 			var date = new Date(item.date*1000)
 			monthDataItems[date.getMonth()].push(item)
 			accountTotals[date.getMonth()] += item.amount
+			item.account = account.name
+			items.push(item)
 		};
 		for (var i = accountTotals.length - 1; i >= 0; i--) {
 			var accountTotal = accountTotals[i]
@@ -96,7 +161,28 @@ function setup(){
 			})
 		};
 	};
-	console.log(monthPieDataSets)
+	//Now deal with weeks:
+	items = items.sort(function(l,r){ return l.date == r.date ? 0 : l.date > r.date ? -1 : 1 })
+	
+	var curWeek = items[0] ? new Date(items[0].date*1000).getWeekNumber() : 0
+	var curDateSet = []
+	var curLabel = items[0] ? "Week of " + new Date(items[0].date*1000).toLocaleDateString() : ("Week #" + weekKey)
+	for (var i = items.length - 1; i >= 0; i--) {
+		var item = items[i]
+		var weekKey = new Date(item.date*1000).getWeekNumber()
+		
+		if(curWeek != weekKey || i == 0){
+			if(i==0){
+				curDateSet.push(item)
+			}
+			weekPieDataSets.push(curDateSet)
+			weekPieLables.push(curLabel)
+			curDateSet = []
+			curLabel = "Week of " + new Date(item.date*1000).toLocaleDateString()
+		}
+		curDateSet.push(item)
+		curWeek = weekKey
+	};
 
 	$('[name=byweek]').click(function(){
 		setupWeekData()
