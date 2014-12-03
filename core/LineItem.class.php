679f05b0ec4d1435755779da9d1b1341dd51252f
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-03 16:32:02 -0500

    Created LineItem and User class

---
+++ core/LineItem.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class LineItem extends Entity {
+
+	public $account_id = NULL;
+	public $name = NULL;
+	public $amount = NULL;
+	public $created_time = NULL;
+
+}
---
+++ core/User.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class User extends Entity {
+	public $ident     = NULL;
+	public $hash      = NULL;
+	public $nickname  = NULL;
+	public $last_seen = NULL;
+}
