b3407417316091c1b4c9807f83739317b0349123
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-04 11:38:59 -0500

    Began work on services.
    Detailed each in revamp.md, trying to keep the concerns away from each
    other as much as possible.
    
    Created authentication service to wrap the password_compat library with.
    Should work to login and etc

--- init.php
+++ init.php
@@ -24,6 +24,12 @@ foreach ($axiomClasses as $axiom) {
 	include $dirname . '/core/'.$axiom.'.class.php';	
 }
 include $dirname . '/bootstrap/database.php';
+include $dirname . '/lib/password_compat/password.php';
+
+$servicesToLoad = array('Authentication');
+foreach ($servicesToLoad as $service) {
+	include $dirname . '/service/'.$service.'Service.php';
+}
 
 /* Attempt to Connect to the database to ensure we can */
 Database::instance();
---
+++ service/AuthenticationService.php
@@ -0,0 +1,84 @@
+<?php
+class AuthenticationService { 
+	private static $instance =NULL;
+	private static $cryptOptions = array("cost" => 11);
+
+	private $db;
+
+	public static function instance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new AuthenticationService();
+		}
+		return self::$instance;
+	}
+
+	private function __construct(){
+		$this->db = Database::instance();
+	}
+
+	public function checkPassword(User $user, $password) {
+		return password_verify($password, $user->hash);
+	}
+
+	private function rehashIfNeccesary(User $user) {
+		if (password_needs_rehash($user->hash, PASSWORD_BCRYPT, self::$cryptOptions)) {
+			$user->hash = password_hash($password, PASSWORD_BCRYPT, self::$cryptOptions);
+			if( ! $this->db->update($user) ) {
+				logMessage('Could not rehash user\'s password [id:' . $user->id . ']');
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/* User passes an entity with nickname and ident filled out 
+	 * we then check that the ident matches the nickname
+	 * the ident is the plain text password, nickname is the username
+	 * after calling this function, $user will be replaced with database 
+	 * if it was successful
+	*/
+	public function canLogin(User &$user) {
+		$matchingUsers = $this->db->where($user, 'nickname', $user->nickname);
+		if ($matchingUsers === false || count($matchingUsers) != 1) {
+			return false;
+		}
+
+		$storedUser = $matchingUsers[0];
+
+		if (!$this->checkPassword($storedUser, $user->ident)) {
+			return false;
+		}
+
+		/* Check if the password needs to be rehashed, 
+		 * if so do so prior to login, if it fails don't login
+		*/
+		if (!$this->rehashIfNeccesary($storedUser)) {
+			return false;
+		}
+
+		$user = $storedUser;
+
+		return true;
+	}
+
+	public function login(User $user) {
+		if($this->canLogin($user)) {
+			session_start();
+			$_SESSION['loggedIn'] = true;
+			$_SESSION['userId'] = $user->id;
+		}
+	}
+
+	public function logout(User $user) { 	
+		$_SESSION = array();
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+		    	$params["secure"], $params["httponly"]
+		    );
+		}
+		session_destroy();
+	}
+
+}
