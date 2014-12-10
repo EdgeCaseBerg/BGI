a1ff84a65b838220a0c12beb41b04d5feab0f7db
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-10 13:36:53 -0500

    Made create line item file
    - not tested yet.

---
+++ api/create-lineitem.php
@@ -0,0 +1,49 @@
+<?php 
+
+$isAjax = isset($_GET['json']) && (intval($_GET['json']) == 1 || $_GET['json'] == 'true');
+$response = new StdClass();
+$response->code = 400;
+$response->message = 'Problem executing request';
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') { 	
+	logMessage("Invalid method", LOG_LVL_DEBUG);
+	if ($isAjax) goto ajax_response;
+	else header('Location: /add-lineitem?e=1');
+    exit();
+}
+
+$invalid = !isset($_POST['account_id']) || empty($_POST['account_id']);
+$invalid = $invalid || !isset($_POST['name']) || empty($_POST['name']);
+$invalid = $invalid || !isset($_POST['amount']) || empty($_POST['amount']);
+if( $invalid ) {
+	if ($isAjax) goto ajax_response;
+	else header('Location: /add-lineitem?e=1');
+    exit();
+}
+
+include '../init.php';
+ensureUserLoggedin(); 
+
+$lineItemService = LineItemService::instance();
+$lineItem = new LineItem();
+$lineItem->account_id = intval($_POST['account_id']);
+$lineItem->name = trim($_POST['name']);
+$lineItem->amount = intval(floatval($_POST['amount'])*100);
+$lineItem->created_time = date('c');
+
+if( $lineItemService->addLineItem($lineItem) === false ) {
+	if ($isAjax) goto ajax_response;
+	else header('Location: /add-lineitem?e=1');
+    exit();	
+}
+
+if ($isAjax) goto ajax_response;
+else header('Location: /add-lineitem');
+exit();
+
+
+ajax_response:
+header('Content-Type: application/json');
+echo json_encode($response);
+exit();
+?>
