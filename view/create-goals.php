322f049e88c40c4f37173976cd25906cf1946522
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-14 21:53:53 -0500

    Added forms for creating goals.
    Need to make the api endpoint to process them next.

--- view/create-goals.php
+++ view/create-goals.php
@@ -38,13 +38,109 @@ if (empty($userAccounts)) {
 	<div class="span-1">
 		<h2>Weekly</h2>
 		<p>Set a goal for a single or multiple accounts in aggregate to meet each week</p>
+		<form class="grid-form" action="/api/create-goal.php" method="POST">
+			<fieldset>
+				<legend>Create Weekly Goal</legend>
+				<div data-row-span="2">
+					<input type="hidden" name="goal_type" value="<?php echo GOAL_TYPE_WEEKLY ?>" />
+					<div data-field-span="1">
+		                <label>Name</label>
+		                <input name="name" type="text">
+		            </div>
+		            <div data-field-span="1">
+		                <label>Amount</label>
+		                <input name="amount" type="text" pattern="[-]?[0-9]+\.[0-9]{2}?" title="Please enter a valid dollar amount">
+		            </div>
+				</div>
+				<div data-row-span="1">
+					<div data-field-span="1">
+		            	<label>Accounts (select multiple)</label>
+						<select name="accounts" multiple >
+							<?php foreach ($userAccounts as $account) {
+								echo '<option value="' . $account->id . '">' . $account->name . '</option>';
+							} ?>
+						</select>
+					</div>
+				</div>
+			</fieldset>
+			<br/>
+			<input type="submit" value="Create" class="button-green bigger right" />
+		</form>
 	</div>
 	<div class="span-1">
 		<h2>Monthly</h2>
 		<p>Set a goal for single or multiple accounts in aggregate to meet each month</p>
+		<form class="grid-form" action="/api/create-goal.php" method="POST">
+			<fieldset>
+				<legend>Create Monthly Goal</legend>
+				<div data-row-span="2">
+					<input type="hidden" name="goal_type" value="<?php echo GOAL_TYPE_MONTHLY ?>" />
+					<div data-field-span="1">
+		                <label>Name</label>
+		                <input name="name" type="text">
+		            </div>
+		            <div data-field-span="1">
+		                <label>Amount</label>
+		                <input name="amount" type="text" pattern="[-]?[0-9]+\.[0-9]{2}?" title="Please enter a valid dollar amount">
+		            </div>
+				</div>
+				<div data-row-span="1">
+					<div data-field-span="1">
+		            	<label>Accounts (select multiple)</label>
+						<select name="accounts" multiple >
+							<?php foreach ($userAccounts as $account) {
+								echo '<option value="' . $account->id . '">' . $account->name . '</option>';
+							} ?>
+						</select>
+					</div>
+				</div>
+			</fieldset>
+			<br/>
+			<input type="submit" value="Create" class="button-green bigger right" />
+		</form>
 	</div>
 	<div class="span-1">
 		<h2>Timed</h2>
 		<p>If you want to set a goal for a specific time period across accounts, use the form below</p>
+		<form class="grid-form" action="/api/create-goal.php" method="POST">
+			<fieldset>
+				<legend>Create Timed Goal</legend>
+				<div data-row-span="2">
+					<input type="hidden" name="goal_type" value="<?php echo GOAL_TYPE_TIMED ?>" />
+					<div data-field-span="1">
+		                <label>Name</label>
+		                <input name="name" type="text">
+		            </div>
+		            <div data-field-span="1">
+		                <label>Amount</label>
+		                <input name="amount" type="text" pattern="[-]?[0-9]+\.[0-9]{2}?" title="Please enter a valid dollar amount">
+		            </div>
+				</div>
+				<div data-row-span="1">
+					<div data-field-span="1">
+		                <label>Start Date</label>
+		                <input name="start_time" type="date">
+		            </div>
+		        </div>
+		        <div data-row-span="1">
+		            <div data-field-span="1">
+		                <label>End Date</label>
+		                <input name="end_time" type="date">
+		            </div>
+				</div>
+				<div data-row-span="1">
+					<div data-field-span="1">
+		            	<label>Accounts (select multiple)</label>
+						<select name="accounts" multiple >
+							<?php foreach ($userAccounts as $account) {
+								echo '<option value="' . $account->id . '">' . $account->name . '</option>';
+							} ?>
+						</select>
+					</div>
+				</div>
+			</fieldset>
+			<br/>
+			<input type="submit" value="Create" class="button-green bigger right" />
+		</form>
 	</div>
 </div>
