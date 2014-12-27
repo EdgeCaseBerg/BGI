5fe40bfb8e1d90dad5ad7b9be0542b7e5880e6e4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-12-27 12:28:46 -0500

    Created endpoint for pulling data for year by weeks.

---
+++ api/get-weekly-data-for-year.php
@@ -0,0 +1,37 @@
+<?php
+
+$response = new StdClass();
+$response->code = 400;
+$response->message = 'Invalid Request';
+include '../init.php';
+
+if ($_SERVER['REQUEST_METHOD'] != 'GET') { 	
+	logMessage('Invalid method', LOG_LVL_DEBUG);
+	goto send_response;
+}
+
+//Todo: add get param to go back further than 1 year
+
+ensureUserLoggedin(); 
+
+$user = new User();
+$user->id = $_SESSION['userId'];
+
+$metricsService = MetricsService::instance();
+
+$results = $metricsService->getLastYearOfData();
+
+$response->code = 200;
+if ($results === false) {
+	$response->data = array();
+	$response->message = 'No data to load';
+} else {
+	$response->data = $results;
+	$response->message = 'Loaded';
+}
+
+
+send_response:
+header('Content-Type: application/json');
+echo json_encode($response);
+exit();
