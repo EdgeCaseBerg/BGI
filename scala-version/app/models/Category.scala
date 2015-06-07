9bbcf7b6b9098d06359e2c2b7363f50749834e88
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-07 18:11:37 -0400

    Add ScalaDoc to Category

--- scala-version/app/models/Category.scala
+++ scala-version/app/models/Category.scala
@@ -1,3 +1,15 @@
 package bgi.models
 
+/** Class representing a category 
+ * 
+ * Categories have a Many to Many relationship with [[LineItem]]s. While the previous 
+ * implementation of bgi only allowed for a single category for each [[LineItem]], 
+ * this one will allow for multiple. This is to allow for more interesting data
+ * and charts. 
+ * 
+ * @param userId The id identifying which user has created this Category
+ * @param name A string describing this category
+ * @param balanceInCents The total balance of line items within this category
+ * @param lastUpdated The time which the balance was last updated for this category
+ */
 case class Category(userId : Long, name: String, balanceInCents: Long, lastUpdated: Long)
