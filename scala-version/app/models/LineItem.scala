8706f53f5ad0f4e4fc5b1ae565f11e7f469e0a80
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-07 18:07:54 -0400

    Add ScalaDoc to LineItem

--- scala-version/app/models/LineItem.scala
+++ scala-version/app/models/LineItem.scala
@@ -1,3 +1,10 @@
 package bgi.models
 
-case class LineItem(accountId: Long, name: String, amountInCents: Long, created_time: Long)
+/** Class representing an individual expendeture  
+ * 
+ * @param userId Id of the user to which this [[Lineitem]] is from
+ * @param name Name of the line item, a brief description
+ * @param amountInCents The amount this item cost in cents
+ * @param createdTime unix epoch timestamp of when this item was made
+ */
+case class LineItem(userId: Long, name: String, amountInCents: Long, createdTime: Long)
