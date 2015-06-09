39c7384fa8cca8666f0d387b684e72adfac2044d
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-09 01:21:38 -0400

    Create Prototyping UserDAO
    Since I haven't picked a database yet, and want to keep things agnostic
    (for fun and a challenge), I'm creating this really simple
    implementation of a datastore within a prototyping package.
    
    Writing basic tests to make sure that things work and I don't run into
    any unexpected weirdness. Tests aren't done yet, but they're coming
    along.

---
+++ scala-version/app/models/dao/prototyping/ProtoUserDAO.scala
@@ -0,0 +1,56 @@
+package bgi.models.dao.prototyping
+
+import bgi.models.dao.UserDAO
+import bgi.models.User
+
+import scala.concurrent.{ExecutionContext, Future, future}
+
+/** A Basic in-memory, Set based datastore to use while prototyping 
+ * 
+ * Good for mocking a DAL in tests or for testing services in isolation. 
+ * Not really meant to be used for production system (hence the name)
+ * And is not thread safe, id's should not be relied on to be unique and 
+ * are subject to race conditions. 
+ */
+class ProtoUserDAO extends UserDAO {
+	val store = scala.collection.mutable.Set[User]()
+	var internalId : Long = 0
+
+	def create(user: User)(implicit ec: ExecutionContext) : Future[Option[User]] = future {
+		/* Kind of silly to have sync in here, but this is a prototyping class */
+		synchronized {
+			val userId = internalId
+			store += user.copy(id = userId) 
+			internalId += 1
+			store.find(_.id == userId)
+		}
+	}
+
+	def findById(id: Long)(implicit ec: ExecutionContext) : Future[Option[User]] = future {
+		store.find(_.id == id)	
+	}
+
+	def remove(id: Long)(implicit ec: ExecutionContext) : Future[Boolean] = future {
+		val toRemove = store.find(_.id == id)
+		toRemove.fold(false){ user => 
+			store -= user
+			!store.exists(_.id == id)
+		}
+	}
+
+	def update(user: User)(implicit ec: ExecutionContext) : Future[Boolean] = future {
+		store.find(_.id == user.id).fold(false) { found => 
+			store -= found 
+			store += user 
+			store.exists(_ == user)	
+		}
+	}
+
+	def incrementLoginAttempt(user: User)(implicit ec: ExecutionContext) : Future[Boolean] = {
+		update(user.copy(loginAttempts = user.loginAttempts + 1))
+	}
+
+	def resetLoginAttempts(user: User)(implicit ec: ExecutionContext) : Future[Boolean] = {
+		update(user.copy(loginAttempts = 0))	
+	}
+}
---
+++ scala-version/test/bgi/models/dao/prototyping/ProtoUserDAOSpec.scala
@@ -0,0 +1,49 @@
+package bgi.models.dao.prototyping
+
+import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+
+import bgi.models._
+
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global 
+
+class ProtoUserDAOSpec extends FlatSpec with ScalaFutures{
+
+	trait DAL {
+		val userDAO = new ProtoUserDAO()
+		val hash = UserPassword("password", UserPasswordComplexity.Normal)
+	}
+	
+	"The ProtoUserDAO" should "create a User" in new DAL {
+		val user = new User(name = "name", hash = hash, id = 0)
+		whenReady(userDAO.create(user)) { dbUser =>
+			assert(dbUser.get == user)
+		}
+	}
+
+	it should "find a created User by ID" in new DAL {
+		val user = new User(name = "name", hash = hash)
+		whenReady(userDAO.create(user)) { dbUser =>
+			whenReady(userDAO.findById(dbUser.get.id)) { found =>
+				assert(found.get.name == user.name)
+				assert(found.get.hash == user.hash)
+			}
+		}
+	}
+
+	it should "assigned each created user a sequential id" in new DAL {
+		val users = List.range(0,10).map( i => new User("name",hash, id = i))
+		val futureUsers : Future[List[Option[User]]] = Future.sequence(users.map(userDAO.create(_)))
+
+		whenReady(futureUsers) { listOfOptionUsers =>
+			val createdIds = listOfOptionUsers.map(_.get.id)
+			for(i <- 0 until 10) {
+				assert(createdIds.contains(i))
+			}
+		}
+	}
+
+	//TODO:  Continue writing tests here...
+		
+}
