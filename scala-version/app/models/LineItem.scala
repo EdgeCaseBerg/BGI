a23a1073ddce1005a92e4a3b27353ebaa8c2191e
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-09 00:22:26 -0400

    Fix typo in scala doc linking

--- scala-version/app/models/LineItem.scala
+++ scala-version/app/models/LineItem.scala
@@ -2,7 +2,7 @@ package bgi.models
 
 /** Class representing an individual expendeture  
  * 
- * @param userId Id of the user to which this [[Lineitem]] is from
+ * @param userId Id of the user to which this [[LineItem]] is from
  * @param name Name of the line item, a brief description
  * @param amountInCents The amount this item cost in cents
  * @param createdTime unix epoch timestamp of when this item was made
