18d33cc4610bc3a1e7ad2db1f7ce538f4b041d50
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-13 14:58:09 -0400

    Create Context object
    Global object to hold instances of dao, services, etc in one place.

---
+++ scala-version/app/global/Context.scala
@@ -0,0 +1,25 @@
+package bgi.globals
+
+import bgi.models.dao.prototyping._
+import bgi.models.dao._
+import bgi.services._
+
+/** Global level dependency injection object
+ *
+ * Subclasses should be used for providing actual Context's to 
+ * controllers or services.
+ */
+abstract trait Context {
+	implicit lazy val userDAO : UserDAO = ???
+	implicit lazy val userService : UserService = ???
+}
+
+/** A Context for using services wired with prototyping (non-db) DAO's 
+ * 
+ * Should not be used in production! This is for testing and trying out 
+ * ideas.
+ */
+trait ProtoContext extends Context{
+	override implicit lazy val userDAO : UserDAO = new ProtoUserDAO()
+	override implicit lazy val userService : UserService = new UserService
+}
