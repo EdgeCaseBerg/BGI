15f685c3d2b9709a707bba620395213d0a5d4663
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-13 14:58:52 -0400

    Create Abstract Dashboard controller
    Creating Abstract controller that will use a Context, but not a specific
    one. This way we can simply extend the controller with the appropriate
    context in order to use those.
    
    It should make it easy to try out new ideas and scope things out
    quickly.

--- scala-version/app/controllers/Dashboard.scala
+++ scala-version/app/controllers/Dashboard.scala
@@ -3,10 +3,16 @@ package bgi.controllers
 import play.api._
 import play.api.mvc._
 
-object Dashboard extends Controller {
+import bgi.services._
+import bgi.globals.Context
+import bgi.globals.ProtoContext
 
+abstract class DashboardController extends Controller with Context {
 	def index = Action {
 		Ok("Hello BGI")
 	}
+}
+
+object Dashboard extends DashboardController with ProtoContext{ 
 
 }
