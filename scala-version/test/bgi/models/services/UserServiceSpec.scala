e40da52a19751f5761bfb002bd4107a0486def01
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-14 09:04:40 -0400

    Add test on rehashing to higher complexities

--- scala-version/test/bgi/models/services/UserServiceSpec.scala
+++ scala-version/test/bgi/models/services/UserServiceSpec.scala
@@ -19,8 +19,9 @@ class UserServiceSpec extends FlatSpec with ScalaFutures{
 			UserPasswordComplexity.Difficult -> """$2a$12$hIAcrb/9H25HM0hmlVc/du2sfmJ89n93rKp6jSqoTDkvrq28B1Wsu""",
 			UserPasswordComplexity.Hard -> """$2a$15$wC8xswg9AqmDUxREqFnmEecFaYj8qSyEWvZ3xbQV5/4aknUslAhCW"""
 		)
+		val awaitTime = scala.concurrent.duration.Duration("1 seconds")
 		val hash = UserPassword(hashes(UserPasswordComplexity.Normal), UserPasswordComplexity.Normal)
-		val user = Await.result(userDAO.create( new User("testUser", hash=hash)), scala.concurrent.duration.Duration("1 seconds")).get
+		val user = Await.result(userDAO.create( new User("testUser", hash=hash)), awaitTime).get
 		val userService = new UserService
 		
 	}
@@ -28,6 +29,15 @@ class UserServiceSpec extends FlatSpec with ScalaFutures{
 	"The UserService" should "authenticate a user if given the correct password" in new TestContext {
 		whenReady(userService.authenticateUser(user, password)) { success =>
 			assert(success)
+		}
+	}
+
+	it should "rehash a password to a higher complexity" in new TestContext {
+		whenReady(userService.rehashWithComplexity(user, "test", UserPasswordComplexity.Difficult)) { success =>
+			assert(success)
+			val updatedUser = Await.result(userService.findUserById(user.id), awaitTime).get
+			assert(updatedUser.hash.hash != user.hash.hash)
+			assert(updatedUser.hash.complexity == UserPasswordComplexity.Difficult)
 		}
 	}
 
