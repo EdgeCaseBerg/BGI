bca4de4eaa5c87e9da014343cfb881a839aab270
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-21 14:16:17 -0400

    Weird issue with SVG d and Fill attributes.
    If I cram everything into the string to pass to `d="` on the svg side,
    it escapes the " ending the d attribute and then includes fill=X
    _inside_ the d attribute instead of rendering as a new attribute. quite
    bizarre.

--- scala-version/app/models/charts/Charts.scala
+++ scala-version/app/models/charts/Charts.scala
@@ -17,19 +17,24 @@ case class Pie(val radius : Int) {
 		}
 	}
 
-	def getSvgArcs : List[String] = {
+	private def determineColor(portion: Double) : String = {
+		"#" + "%06x".format((portion.toInt*0xf24) % 0xffffff ).replace("0","f")
+	}
+
+	def getSvgArcs : List[(String,String)] = {
 		val allPortions = portions.toList
 		val total = allPortions.reduce { (l,r) => l + r }
 		var prevX = radius.toDouble
 		var prevY = 0.0
 		var arcLength = 0.0
-		val paths = scala.collection.mutable.ListBuffer[String]()
+		val paths = scala.collection.mutable.ListBuffer[(String, String)]()
 		for(portion <- allPortions) {
 			var largeArcFlag = determineLargeArcFlag(portion, total)
 			arcLength = (portion/total) * 360 + arcLength
 			val nextX = cos(arcLength.toRadians) * radius
 			val nextY = sin(arcLength.toRadians) * radius
-			paths += s"l ${prevX} ${-prevY} a ${radius} ${radius} 0, ${largeArcFlag} 0 ${nextX - prevX} ${-(nextY - prevY)} z"
+			val color = determineColor(portion)
+			paths += Tuple2(s"l ${prevX} ${-prevY} a ${radius} ${radius} 0, ${largeArcFlag} 0 ${nextX - prevX} ${-(nextY - prevY)} z", color)
 			prevX = nextX
 			prevY = nextY
 		}
