8c609d68abc09a52094b4430f8ecf5d9a5effa43
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-21 23:16:35 -0400

    Removing Login/Register Views and Handlers
    Since I'm the only one who uses the software, I'm just not going to
    bother with a user system

--- scala-version/app/controllers/Dashboard.scala
+++ scala-version/app/controllers/Dashboard.scala
@@ -3,7 +3,6 @@ package bgi.controllers
 import play.api._
 import play.api.mvc._
 
-import bgi.forms._
 import bgi.models._
 import bgi.services._
 import bgi.globals.Context
@@ -18,27 +17,6 @@ import scala.concurrent.Future
 abstract class DashboardController extends Controller with Context {
 	def index = Action { implicit request =>
 		Ok(views.html.index())
-	}
-
-	def register = Action.async { implicit request =>
-		RegisterForm.form.bindFromRequest.fold(
-			formWithErrors => {
-				/* We don't actually want to tell them what went wrong :^) */
-				Future.successful(
-					Redirect("/").flashing("error" -> "Error! Please ensure your username and password are at least 4 characters long and you've entered the right admin code")
-				)
-			},
-			boundForm => {
-				val newUser = new User(name = boundForm.username, hash = UserPassword(BCrypt.hashpw(boundForm.password, BCrypt.gensalt(UserPasswordComplexity.Normal)), UserPasswordComplexity.Normal))
-				val createdUserFuture = userService.createUser(newUser)
-				for {
-					createdUser <- createdUserFuture
-				} yield createdUser
-				createdUserFuture.map { user =>
-					Redirect("/").flashing("success" -> "Successfully created user, please sign in!")
-				}
-			}
-		)
 	}
 
 	def test = Action { 
