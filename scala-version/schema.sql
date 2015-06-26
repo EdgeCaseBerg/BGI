71351de2e93b94fcdd8d8f04d0a2eb83adbf135c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-26 00:18:43 -0400

    Fix schema to have hash column in users

--- scala-version/schema.sql
+++ scala-version/schema.sql
@@ -7,6 +7,7 @@ USE bgi;
 CREATE TABLE users (
 	id INT(12) NOT NULL auto_increment PRIMARY KEY,
 	name VARCHAR(64) NOT NULL,
+	hash VARCHAR(256) NOT NULL,
 	complexity INT(2) NOT NULL DEFAULT 10, 
 	email VARCHAR(128),
 	loginAttempts INT(2) NOT NULL DEFAULT 0
