4e390fd1f324a156a2385c1ddd627634f7b45c46
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-27 14:10:28 -0400

    ScalaDoc for Authenticated.

--- scala-version/app/global/Authenticated.scala
+++ scala-version/app/global/Authenticated.scala
@@ -10,9 +10,14 @@ import scala.util.{Try, Success, Failure}
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
+/** Class representing a request that has been authenticated 
+ * 
+ * @param user The user who is authenticated
+ * @param request The current request
+ */
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
+/** Action to use in controllers that require a logged in user */
 object Authenticated extends ActionBuilder[AuthenticatedRequest] with AnormContext{
 	val failRedir = Future.successful(Results.Redirect("/").flashing("error" -> "Please Login and have cookies enabled"))
 
