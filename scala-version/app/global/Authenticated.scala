8684edeb97e1bf51e5fb13de79c5eddd5688fbc1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-27 14:07:57 -0400

    Formatting

--- scala-version/app/global/Authenticated.scala
+++ scala-version/app/global/Authenticated.scala
@@ -14,22 +14,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
 
 object Authenticated extends ActionBuilder[AuthenticatedRequest] with AnormContext{
+	val failRedir = Future.successful(Results.Redirect("/").flashing("error" -> "Please Login and have cookies enabled"))
 
-  def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
-    request.session.get("userId").map { stringUserId =>
-    	Try(stringUserId.toLong) match {
-    		case Failure(e) => Future.successful(Results.Forbidden("You must login"))
-    		case Success(userId) => 
-    			val possibleUser : Future[Option[User]] = userService.findUserById(userId)
-    			possibleUser.flatMap { optionUser =>
-    				optionUser match { 
-    					case Some(user) => block(new AuthenticatedRequest(user, request))
-    					case _ => Future.successful(Results.Forbidden("You must login"))
-    				}	
-    			}
-    	}
-    }.getOrElse {
-      Future.successful(Results.Forbidden("You must login"))
-    }
-  }
+	def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
+		request.session.get("userId").map { stringUserId =>
+			Try(stringUserId.toLong) match {
+				case Failure(e) => failRedir
+				case Success(userId) => 
+					val possibleUser : Future[Option[User]] = userService.findUserById(userId)
+					possibleUser.flatMap { optionUser =>
+						optionUser match { 
+							case Some(user) => block(new AuthenticatedRequest(user, request))
+							case _ => failRedir
+						}	
+					}
+			}
+		}.getOrElse {
+		  failRedir
+		}
+	}
 }
