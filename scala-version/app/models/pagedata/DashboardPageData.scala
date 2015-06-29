648edab7f8c88985a732309297747549b927cd6a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-06-28 23:52:59 -0400

    Scaladoc the dashboard data

--- scala-version/app/models/pagedata/DashboardPageData.scala
+++ scala-version/app/models/pagedata/DashboardPageData.scala
@@ -8,6 +8,7 @@ import bgi.models.charts._
  * @note At a future date, might refactor preferredCategories to a List of Lists
  *
  * @param recentItems A list of recent line items
- * @param preferredCategories a subset of all categories that the user wants to see a chart of
+ * @param categories all categories, used for the create line item form
+ * @param charts charts to display on the view. 
  */
-case class DashboardPageData(recentItems: List[LineItem], preferredCategories: List[Category], charts: List[Pie])
+case class DashboardPageData(recentItems: List[LineItem], categories: List[Category], charts: List[Pie])
