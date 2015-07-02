9e67e96d5dfaa0b3e5157708eba6b3c36e6209cc
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-01 23:45:54 -0400

    Add categories page.
    PageData to contain the categories specific information.
    Update routes file with new path to method
    Add categories to dashboard controller. At this point it might be good
    to just rename it to Application or something similar.

---
+++ scala-version/app/models/pagedata/CategoryPageData.scala
@@ -0,0 +1,9 @@
+package bgi.models.pagedata
+
+import bgi.models._
+
+/** Data for the Cateogory page
+ * 
+ * @param categories all categories in the system
+ */
+case class CategoryPageData(categories: List[Category])
