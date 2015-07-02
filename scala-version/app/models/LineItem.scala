cab80cd9f33ce57d6dc18b360eebf0270db6c0aa
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-01 22:52:32 -0400

    Add lineItem partial and helper functions
    Add view partial so we can be consistent in the viewing of lineItem
    lists, not sure if the spans will stay or not, just kind of making
    things up as I go and didn't want to commit to using a table yet.

--- scala-version/app/models/LineItem.scala
+++ scala-version/app/models/LineItem.scala
@@ -1,5 +1,8 @@
 package bgi.models
 
+import java.text.NumberFormat
+import org.joda.time.DateTime
+
 /** Class representing an individual expendeture  
  * 
  * @param userId Id of the user to which this [[LineItem]] is from
@@ -7,4 +10,14 @@ package bgi.models
  * @param amountInCents The amount this item cost in cents
  * @param createdTime unix epoch timestamp of when this item was made
  */
-case class LineItem(id: Long, userId: Long, categoryId: Long, name: String, amountInCents: Long, createdTime: Long)
+case class LineItem(id: Long, userId: Long, categoryId: Long, name: String, amountInCents: Long, createdTime: Long) {
+	def amountInDollars : String = {
+		val numberFormat = NumberFormat.getCurrencyInstance()
+		numberFormat.format(amountInCents/100.0)
+	}
+
+	def stringDate : String = {
+		new DateTime(createdTime*1000).toLocalDate.toString
+	}
+
+}
