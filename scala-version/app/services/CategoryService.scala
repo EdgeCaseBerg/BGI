4b2386408fbb2443b609a0d882c64768b51c68dc
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-05 11:47:43 -0400

    Add Category Service

---
+++ scala-version/app/services/CategoryService.scala
@@ -0,0 +1,29 @@
+package bgi.services
+
+import bgi.models._
+import bgi.models.dao.CategoryDAO
+
+import scala.concurrent.{Future, future}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+/** Service abstraction over operations involving the Categories
+ *
+ * Provides layer over basic DAO and ability to perform business logic
+ * surrounding the category object.
+ */
+class CategoryService(implicit val categoryDAO : CategoryDAO) extends BaseService[Category]{
+	type Categories = List[Category]
+	
+	/** Returns all line items 
+	 *
+	 * @return A Future of a list of Categories in the system
+	 */
+	def getAll : Future[Categories] = categoryDAO.getAll()
+
+	/** Retrieve all the preffered categories from the database 
+	 *
+	 * @return A Future of a list of Categories that are preffered in the system
+	 */
+	def getPreffered : Future[Categories] = categoryDAO.getPreffered()	
+}
