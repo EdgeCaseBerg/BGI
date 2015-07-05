2a4ff4c246791bb473df8f2fb494f69c3c00c85e
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-05 11:22:45 -0400

    Category DAO
    Not sure if this will be the DAO's final form, but for now I think I'm
    just going to assume that the preffered categories are just a single
    subset instead of multiple. I just want to make things simple and get
    some operations going on the page before I really work out the multiple
    preffered categories.
    
    I also need a better name for it than preffered categories... Groups?
    Category groups? That might sound better.

---
+++ scala-version/app/models/dao/CategoryDAO.scala
@@ -0,0 +1,29 @@
+package bgi.models.dao
+
+import bgi.models._
+
+import scala.concurrent.{Future, ExecutionContext}
+
+/** Category specific DAO operations 
+ * 
+ * All functions take an implicit ExecutionContext which is used to 
+ * run the future threads in. 
+ */
+trait CategoryDAO extends CrudDAO[Category] {
+	type Categories = List[Category]
+
+	/** Retrieves all Categories
+	 *
+	 * @param ec Implicit ExecutionContext to run future threads in
+	 * @return A Future containing a list of Categories matching the tag
+	 */
+	def getAll()(implicit ec: ExecutionContext) : Future[Categories]
+
+	/** Retrieves all Categories that are preffered by the user
+	 * 
+	 * @param ec Implicit ExecutionContext to run function threads in
+	 * @return A Future containing a list of Categories matching the categories provided
+	 */
+	def getPreffered()(implicit ec: ExecutionContext) : Future[Categories]
+	
+}
