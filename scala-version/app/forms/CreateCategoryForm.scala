fe92ba43dd72b15e44c485a0407be08a6c786fa0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-05 17:05:30 -0400

    CreateCategoryForm
    Form model to bind to requests. I wonder if there is a way to override
    the bind from request to retrieve the user from underneath the request
    and perform a validation check against the session

---
+++ scala-version/app/forms/CreateCategoryForm.scala
@@ -0,0 +1,24 @@
+package bgi.forms
+
+import play.api.data._
+import play.api.data.Forms._
+
+/** Container for the category creation form 
+ * 
+ * @param name The submitted category name, a string length 3-64
+ * @param userId The id of the User to create the Category for
+ */
+case class CreateCategoryForm(name: String, userId: Long) 
+
+/** Companion object of the category creation form 
+ *
+ * Provides a form object to bind requests and validate form input
+ */
+object CreateCategoryForm {
+	val form = Form(
+		mapping(
+			"name" -> nonEmptyText(minLength = 3, maxLength = 64),
+			"userId" -> longNumber
+		)(CreateCategoryForm.apply)(CreateCategoryForm.unapply)
+	)
+}
